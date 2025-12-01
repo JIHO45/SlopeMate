f7e93572c54bbd8a390cd021ad53f5d1b394454c
Joe <pjh040217@naver.com>
2025-12-01 21:37:02 +0900

    Initial Commit

---
+++ SlopeMate/SlopeMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlopeMateApp.swift
+//  SlopeMate
+//
+//  Created by 박지호 on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SlopeMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SlopeMateTests/SlopeMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  SlopeMateTests.swift
+//  SlopeMateTests
+//
+//  Created by 박지호 on 12/1/25.
+//
+
+import Testing
+@testable import SlopeMate
+
+struct SlopeMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
